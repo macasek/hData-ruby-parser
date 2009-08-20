d81fe83bdfc24afddf08fc1dc33faedc28696f07
Michael Macasek <mmacasek@mitre.org>
2009-08-20 08:48:30 -0400

    mod for namespace oddities with orgs

--- lib/models/core_data_types/organization.rb
+++ lib/models/core_data_types/organization.rb
@@ -1,7 +1,5 @@
 class Organization < XMLBase
-  xml_namespace :hdataCoreNs
-  
   xml_accessor :name
-  xml_accessor :addresses, :as => [Address]
+  xml_accessor :addresses, :as => [Address], :from => "hdataCoreNs:address"
   xml_accessor :points_of_contacts, :as => [Person], :from => "pointOfContacts" 
 end
